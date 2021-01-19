a70d9c90d320dfa9c6275d5b19085eebaa51872c
Gnarlyluck <schuylerluckey@gmail.com>
2021-01-19 10:47:40 -0800

    imported search bar from material-UI but have to get it working.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,20 +3,86 @@ import {NavLink} from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 import '../styles/Nav.css'
-import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import DehazeIcon from '@material-ui/icons/Dehaze';
-
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import LocalBarIcon from '@material-ui/icons/LocalBar';
 import CreateTwoToneIcon from '@material-ui/icons/CreateTwoTone';
 import PlayForWorkIcon from '@material-ui/icons/PlayForWork';
 import { green, yellow } from '@material-ui/core/colors';
 
+const useStyles = makeStyles((theme) => ({
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+    sectionDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+      },
+    },
+    sectionMobile: {
+      display: 'flex',
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      },
+    },
+  }));
+
 const Nav = ({authenticate, currentUser}) => {
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const classes = useStyles();
+
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
       };
@@ -28,9 +94,7 @@ const Nav = ({authenticate, currentUser}) => {
             <nav>
                 <span >
                     <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                        {/* <MoreVertIcon /> */}
                         <DehazeIcon style={{ color: yellow[200]}}/>
-                        {/* Options */}
                     </IconButton>
                     <Menu
                         id="simple-menu"
@@ -45,33 +109,33 @@ const Nav = ({authenticate, currentUser}) => {
                         <Link to={'showcategory'} style={{textDecoration: 'none', color: 'black'}}>
                             <MenuItem>Search By Category</MenuItem>
                         </Link>
-                        <Link to={'/'}style={{textDecoration: 'none', color: 'black'}}>
-                            <MenuItem onClick={() => {localStorage.clear()}} >Sign Out</MenuItem>
-                        </Link>
                         <Link to={'/profile'} style={{textDecoration: 'none', color: 'black'}}>
                             <MenuItem>Feed</MenuItem>
                         </Link>
                         <Link to={'/search'} style={{textDecoration: 'none', color: 'black'}}>
                             <MenuItem>Look up cocktail</MenuItem>
                         </Link>
+                        <Link to={'/'}style={{textDecoration: 'none', color: 'black'}}>
+                            <MenuItem onClick={() => {localStorage.clear()}} >Sign Out</MenuItem>
+                        </Link>
                     </Menu>
                 </span>
-        
-               
-               
-                {/* <NavLink 
-                to='/'
-                onClick={() => { localStorage.clear() }}
-                >
-                    <h3> Sign Out</h3>
-                </NavLink> */}
-                {/* <NavLink to='/showcategory'>
-                    Search by Category
-                </NavLink>
-                <NavLink to='/Create'>
-                    Create Post
-                </NavLink>
-                <NavLink to='/profile'>Home</NavLink> */}
+                <span>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon style={{color: '#faf390'}}/>
+                        </div>
+                            <InputBase
+                            placeholder="Look up Cocktails"
+                            style={{color: '#faf390'}}
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                            />
+                    </div>
+                </span>
                 <h1 >COCKTAIL SOCIAL {<LocalBarIcon/>}</h1>
             </nav>
         </header>
